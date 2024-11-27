c33ecd3299c713da62abded0e3fff6f71e4a8a58
M.Rizqullah <mm.rizqullah@gmail.com>
2024-11-27 23:39:47 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mob3_uas_klp_04/views/screens/authentification_screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:mob3_uas_klp_04/views/screens/authentification_screens/register_screen.dart';
@@ -21,6 +22,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: RegisterScreen(),
