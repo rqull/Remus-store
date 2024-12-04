d21511e96707288668df100748eef884a30c02a0
M.Rizqullah <mm.rizqullah@gmail.com>
2024-12-04 21:57:19 +0800

    Refactor BannerWidget to use BannerController and StreamBuilder for dynamic banner loading
    - Replaced FirebaseFirestore integration with BannerController for improved data handling.
    - Implemented StreamBuilder to asynchronously fetch and display banner images.
    - Enhanced UI with padding, container styling, and error handling for better user experience.

--- lib/views/screens/nav_screens/widgets/banner_widget.dart
+++ lib/views/screens/nav_screens/widgets/banner_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mob3_uas_klp_04/controllers/banner_controller.dart';
 
 // Kelas BannerWidget adalah widget stateful yang menampilkan banner
 class BannerWidget extends StatefulWidget {
@@ -11,48 +11,62 @@ class BannerWidget extends StatefulWidget {
 
 // State untuk BannerWidget
 class _BannerWidgetState extends State<BannerWidget> {
-  // Instance dari FirebaseFirestore untuk mengakses koleksi Firestore
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  // List untuk menyimpan URL gambar banner
-  final List _bannerImage = [];
-
-  // Fungsi untuk mengambil banner dari Firestore
-  getBanners() {
-    return _firestore.collection('banners').get().then(
-      (QuerySnapshot querySnapshot) {
-        // Iterasi melalui dokumen yang diambil
-        querySnapshot.docs.forEach(
-          (doc) {
-            setState(() {
-              // Menambahkan URL gambar ke dalam list _bannerImage
-              _bannerImage.add(doc['image']);
-            });
-          },
-        );
-      },
-    );
-  }
-
-  @override
-  void initState() {
-    // Memanggil getBanners untuk mengambil data banner saat inisialisasi
-    getBanners();
-    super.initState();
-  }
+  final BannerController _bannerController = BannerController();
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 140,
-      width: MediaQuery.of(context).size.width,
-      child: PageView.builder(
-        itemCount: _bannerImage.length,
-        itemBuilder: (context, index) {
-          return Image.network(
-            _bannerImage[index],
-          );
-        },
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 170,
+        decoration: BoxDecoration(
+          color: Color(0XFFF7F7F7),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 5,
+              blurRadius: 10,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        child: StreamBuilder<List<String>>(
+          stream: _bannerController.getBannerUrls(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: Colors.blue,
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Icon(Icons.error);
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Center(
+                child: Text(
+                  "No Banners Availabel",
+                  style: TextStyle(fontSize: 10, color: Colors.grey),
+                ),
+              );
+            } else {
+              return Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  PageView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return Image.network(
+                        snapshot.data![index],
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  )
+                ],
+              );
+            }
+          },
+        ),
       ),
     );
   }
