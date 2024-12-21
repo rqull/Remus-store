24e9a24fd951882f78ff3f68daef2324684d7d2f
M.Rizqullah <mm.rizqullah@gmail.com>
2024-12-21 22:16:08 +0800

    Enhance ProductDetailScreen and ProductItemWidget with improved layout and image handling
    - Refactored ProductDetailScreen to include a horizontal image carousel using PageView.builder for better visual presentation of product images.
    - Updated styling for product details, including name, price, and category, to enhance UI consistency and appeal.
    - Modified ProductItemWidget to use 'productImages' for image loading, ensuring consistency across the application.
    - Improved overall user experience with better tap handling and layout organization.

--- lib/views/screens/nav_screens/widgets/product_item_widget.dart
+++ lib/views/screens/nav_screens/widgets/product_item_widget.dart
@@ -150,7 +150,7 @@ class ProductItemWidget extends StatelessWidget {
                       left: 10,
                       top: -10,
                       child: CachedNetworkImage(
-                        imageUrl: productData['productImage'][0],
+                        imageUrl: productData['productImages'][0],
                         width: 108,
                         height: 107,
                       ),
