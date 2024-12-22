de0ef862d3110479976bd21894c95261c1e4792d
M.Rizqullah <mm.rizqullah@gmail.com>
2024-12-22 20:24:02 +0800

    Update dependencies and enhance CartScreen UI with badges
    - Added 'badges' package as a dependency in pubspec.yaml for improved cart item notification.
    - Updated pubspec.lock to include the new 'badges' package version 3.1.2.
    - Refactored CartScreen to integrate badges for displaying the number of items in the cart, enhancing user experience.
    - Improved the app bar layout with a custom background image and better positioning of cart elements.

--- lib/views/screens/nav_screens/cart_screen.dart
+++ lib/views/screens/nav_screens/cart_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:badges/badges.dart' as badges;
 import '../../../provider/cart_provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CartScreen extends ConsumerStatefulWidget {
   const CartScreen({super.key});
@@ -16,14 +17,76 @@ class _CartScreenState extends ConsumerState<CartScreen> {
   Widget build(BuildContext context) {
     final cartData = ref.watch(cartProvider);
     return Scaffold(
+        appBar: PreferredSize(
+          preferredSize:
+              Size.fromHeight(MediaQuery.of(context).size.height * 0.2),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 118,
+            clipBehavior: Clip.hardEdge,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/icons/cartb.png'),
+                  fit: BoxFit.cover),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 322,
+                  top: 52,
+                  child: Stack(
+                    children: [
+                      Image.asset(
+                        'assets/icons/not.png',
+                        width: 26,
+                        height: 26,
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: badges.Badge(
+                          badgeStyle: badges.BadgeStyle(
+                              badgeColor: Colors.yellow.shade800),
+                          badgeContent: Text(
+                            cartData.length.toString(),
+                            style: GoogleFonts.lato(
+                              textStyle: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  left: 61,
+                  top: 51,
+                  child: Text(
+                    'My Cart',
+                    style: GoogleFonts.lato(
+                      textStyle: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
         body: ListView.builder(
-      shrinkWrap: true,
-      physics: ScrollPhysics(),
-      itemCount: cartData.length,
-      itemBuilder: (context, index) {
-        final cartItem = cartData.values.toList()[index];
-        return Center(child: Text(cartItem.productName));
-      },
-    ));
+          shrinkWrap: true,
+          physics: const ScrollPhysics(),
+          itemCount: cartData.length,
+          itemBuilder: (context, index) {
+            final cartItem = cartData.values.toList()[index];
+            return Center(child: Text(cartItem.productName));
+          },
+        ));
   }
 }
